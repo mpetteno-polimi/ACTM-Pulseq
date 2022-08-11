11066b3ff74a733b0b4420500daad6e9ccf6c5a2
Matteo Pettenò <matteo.petteno@mail.polimi.it>
2022-08-11 19:48:00 +0200

    - TODO

--- src/Pulseq.svelte
+++ src/Pulseq.svelte
@@ -31,7 +31,6 @@
 <style>
 
     :root {
-        /* TODO - Check font */
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
         'Helvetica Neue', sans-serif;
     }
